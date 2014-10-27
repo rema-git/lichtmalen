2c824168fe2c351cca1cb5e6590790ffec3f158a
Reinhardt A.W. Maier <rema-git@users.noreply.github.com>
2014-10-27 14:22:15 +0100

    Create jpg_to_tpm2.py
    initial upload

---
+++ jpg_to_tpm2.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 27 00:33:17 2014
+
+@author: Reinhardt A.W. Maier <rema@zaehlwerk.net>
+"""
+import sys
+import numpy as np
+import Image
+#import matplotlib.pyplot as plt
+
+def tpm2(image):
+    """
+    generate TMP2 file format:
+     * image as numpy array with dim(height, width, color)
+     * returns tpm2 string
+    """
+    dim = tuple((np.shape(image)[0], np.shape(image)[1]))
+    framesize = '{:04X}'.format(dim[1]*3)
+    frameheader = 'C9 DA ' + ' '.join((framesize[:2], framesize[2:]))
+    output = [] # file header
+
+    for frame in range(dim[0]): # loop over lines = height
+        output += frameheader
+        for led in range(dim[1]): # loop over columns = width
+            output += ' {:02X} {:02X} {:02X}'.format(*image[frame][led])
+        output += ' 36 ' # end-of-frame
+    output += '\n' # file footer, EOF
+
+    return ''.join(output)
+
+def imageFilter(image):
+    """
+    example filter function
+    """
+    filteredImage = image.resize((128, 128))
+
+    return filteredImage 
+
+# show usage info
+if len(sys.argv) < 3:
+    print 'Usage: jpg_to_tpm2.py foo.jpg bar.tp2'
+    sys.exit(1)
+
+# open jpg file
+try:
+    filename = sys.argv[1]
+    image = Image.open(filename)
+    print 'Image file read.'
+except:
+    print 'ERROR: cannot read .jpg file!'
+
+# filter image
+image = imageFilter(image)
+
+# convert to numpy array with dim(height, width, color)
+image = np.array(image)
+
+# display image
+#plt.imshow(image, interpolation='none')
+
+# convert image to tmp2
+tmp2string = tpm2(image)
+print 'Image converted.'
+
+# show result to screen
+#print '\n' + tmp2string
+
+# write result to file
+tmp2file = sys.argv[2]
+with open(tmp2file, 'w') as text_file:
+    text_file.write(tmp2string)
+    print 'TPM2 file written.'
